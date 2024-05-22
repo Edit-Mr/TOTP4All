cf3db9788274b438d13ddaeb0a7f93f9bfc7c02b
Edit-Mr <elvismao.070512@gmail.com>
2024-05-22 23:11:01 +0800

    add font and more

--- index.js
+++ index.js
@@ -7,25 +7,28 @@ const path = require('path');
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-const db = new JsonDB(new Config(path.join(__dirname, '/token.json'), true, true, '/'))
 
-// static folder to /static
+const db = new JsonDB(new Config(path.join(__dirname, '/token.json'), true, true, '/'));
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'templates'));
+
+let fakeCode = {};
+let cleanCodeStart = false;
+
 app.use('/static', express.static('static'));
 
-//home page
+// 主页
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/templates/home.html');
 });
 
-// Endpoint to add a TOTP secret
-app.post('/api/totp', (req, res) => {
+app.post('/create', (req, res) => {
     const { secret, name } = req.body;
     let keyName = name;
-
     if (!secret) {
         return res.status(400).send("Secret is required.");
     }
-
     if (!keyName) {
         keyName = Math.random().toString(36).substring(2, 12);
     }
@@ -33,40 +36,58 @@ app.post('/api/totp', (req, res) => {
     if (data.hasOwnProperty(keyName)) {
         console.log("WTF");
         console.log(data);
-        // If no error is thrown, it means the key already exists
+        // 如果没有抛出错误，说明密钥已存在
         return res.status(400).send("Key name already exists.");
     } else {
-        // Path does not exist, so create a new key
+        // 路径不存在，所以创建一个新的密钥
         db.push(`/totp/${keyName}`, { secret });
-        // Optionally print out the database for debugging
+        // 可选地打印出数据库以进行调试
         console.log(db.getData("/totp"));
         res.send({ keyName, message: "Secret saved successfully." });
     }
-
 });
 
-// Endpoint to retrieve the TOTP code
 app.get('/totp/:keyname', (req, res) => {
     const keyName = req.params.keyname;
-
-    try {
-        const data = db.getData(`/totp/${keyName}`);
-        const token = speakeasy.totp({
-            secret: data.secret,
+    const data = db.getData(`/totp/`);
+    let token = '';
+    if (data.hasOwnProperty(keyName)) {
+        token = speakeasy.totp({
+            secret: data[keyName].secret,
             encoding: 'base32'
         });
-        res.render('totp', { token });
-
-    } catch (error) {
-        res.status(404).send("Key not found.");
+    } else if (fakeCode.hasOwnProperty(keyName)) {
+        token = fakeCode[keyName];
+    } else {
+        if (!cleanCodeStart) {
+            clearFakeCode();
+            cleanCodeStart = true;
+        }
+        token = Math.floor(Math.random() * 1000000).toString().padStart(6, '0');
+        fakeCode[keyName] = token;
+        console.log(fakeCode);
     }
+    res.render('totp', { token });
 });
 
 app.get('/create', (req, res) => {
     res.sendFile(__dirname + '/templates/create.html');
 });
 
+const clearFakeCode = () => {
+    const countdown = 30 - new Date().getSeconds() % 30;
+    setTimeout(() => {
+        fakeCode = {};
+        cleanCodeStart = false;
+    }, countdown * 1000);
+}
+
+//404
+app.use((req, res, next) => {
+    res.status(404).sendFile(__dirname + '/templates/404.html');
+});
 const PORT = 3000;
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
+
